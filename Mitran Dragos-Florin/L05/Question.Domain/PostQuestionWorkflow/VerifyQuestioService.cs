f97f9c56db2d1892c77b703f1ad485c83156b19b
dragosm99 <63775686+dragosm99@users.noreply.github.com>
2020-10-28 20:38:12 +0000

    Add files via upload

---
+++ Mitran Dragos-Florin/L05/Question.Domain/PostQuestionWorkflow/VerifyQuestioService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LanguageExt.Common;
+using static Question.Domain.PostQuestionWorkflow.Question;
+
+namespace Question.Domain.PostQuestionWorkflow
+{
+    public class VerifyQuestioService
+    {
+        public Result<VerifiedQuestion> VerifyQuestion(UnverifiedQuestion question)
+        {
+            return new VerifiedQuestion(question.Question, question.Tags);
+        }
+    }
+}
